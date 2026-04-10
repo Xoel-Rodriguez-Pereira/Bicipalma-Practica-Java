a2d6417f7ce6660e9d7de9babc3fdd1ac6fddd7a
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-04-10 12:37:14 +0200

    build: Iniciar proyecto y configurar pom.xml

---
+++ src/main/java/edu/teamrocket/bicipalma/App.java
@@ -0,0 +1,10 @@
+package edu.teamrocket.bicipalma;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
