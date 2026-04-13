e3d8c3ea1356a35aaef200527cec9d23acf8c00c
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-04-13 13:00:15 +0200

    feat: Añadir logica clase TarjetaUsuario

---
+++ src/main/java/edu/teamrocket/bicipalma/domain/usuario/TarjetaUsuario.java
@@ -0,0 +1,25 @@
+package edu.teamrocket.bicipalma.domain.usuario;
+
+class TarjetaUsuario {
+    
+    private final String id;
+    private boolean activada = false;
+
+    TarjetaUsuario(String id, boolean activada) {
+        this.id = id;
+        this.activada = activada;
+    }
+    
+    public boolean isActivada() {
+        return this.activada;
+    }
+
+    public void setActivada(boolean estado) {
+        this.activada = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarjeta: %I -- %B".formatted(this.id, this.activada);
+    }
+}
